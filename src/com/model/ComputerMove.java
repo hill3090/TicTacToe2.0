17fe8b0ee5d15fda0e7973f9c634ed47dee6ef0d
Andrew Hill <hill3090@gmail.com>
2021-06-16 21:31:01 -0400

    AI prototype added

---
+++ src/com/model/ComputerMove.java
@@ -0,0 +1,23 @@
+package com.model;
+
+public class ComputerMove {
+
+    int row;
+    int col;
+
+    public int getRow() {
+        return row;
+    }
+
+    public void setRow(int row) {
+        this.row = row;
+    }
+
+    public int getColumn() {
+        return col;
+    }
+
+    public void setColumn(int column) {
+        this.col = column;
+    }
+}
--- src/com/model/Model.java
+++ src/com/model/Model.java
@@ -6,6 +6,7 @@ import java.util.Random;
 public class Model {
 
     private char[][] board;
+    ComputerMove compMove;
     private int boarddim;
     private int movesCount;
     private int won;
@@ -31,7 +32,17 @@ public class Model {
         movesCount = 9;
         playerOne = new Player("Andrew", 123,true);
         computer = new Player("Tori", 345,false);
+        compMove = new ComputerMove();
 
+    }
+
+    public boolean isMovesLeft(char board[][])
+    {
+        for (int i = 0; i < 3; i++)
+            for (int j = 0; j < 3; j++)
+                if (board[i][j] == '_')
+                    return true;
+        return false;
     }
 
     public void registerView(View v) {
@@ -63,6 +74,8 @@ public class Model {
                     return;
                 }
             }
+
+            /*
             random = rand.nextInt(boarddim);
             randomTwo = rand.nextInt(boarddim);
             while (board[random][randomTwo] != '\0')    {
@@ -72,6 +85,15 @@ public class Model {
             board[random][randomTwo] = 'O';
             view.computerClick(random, randomTwo);
             movesCount--;
+            */
+
+            compMove = findBestMove(board);
+            board[compMove.row][compMove.col] = 'O';
+            movesCount--;
+            view.computerClick(compMove.row, compMove.col);
+
+
+            // Not sure why this is here....
             view.updateView(x, y, board[x][y], "Player One's Turn");
 
             won = checkWin(x, y);
@@ -90,8 +112,9 @@ public class Model {
     }
 
     public void updateBoardComputer(int x, int y)   {
-        board[random][randomTwo] = 'O';
+        board[compMove.row][compMove.col] = 'O';
         view.updateView(x, y, board[x][y], "Player One's Turn");
+        playerTurns();
 
         won = checkWin(x, y);
         if (won == -1) {
@@ -105,6 +128,174 @@ public class Model {
                 view.gameWon(computer.getName() + " has won.");
         }
     }
+
+    public ComputerMove findBestMove(char board[][])    {
+
+        int bestVal = -1000;
+        ComputerMove bestMove = new ComputerMove();
+        bestMove.row = -1;
+        bestMove.col = -1;
+
+        for (int i = 0; i < 3; i++)
+        {
+            for (int j = 0; j < 3; j++)
+            {
+                // Check if cell is empty
+                if (board[i][j] == '\0')
+                {
+                    // Make the move
+                    board[i][j] = 'X';
+
+                    // compute evaluation function for this
+                    // move.
+                    int moveVal = minimax(board, 0, false);
+
+                    // Undo the move
+                    board[i][j] = '\0';
+
+                    // If the value of the current move is
+                    // more than the best value, then update
+                    // best/
+                    if (moveVal > bestVal)
+                    {
+                        bestMove.row = i;
+                        bestMove.col = j;
+                        bestVal = moveVal;
+                    }
+                }
+            }
+        }
+
+        return bestMove;
+    }
+
+    public int minimax(char board[][], int depth, boolean isMax)    {
+        int score = evaluate(board);
+
+        // If Maximizer has won the game
+        // return his/her evaluated score
+        if (score == 10)
+            return score;
+
+        // If Minimizer has won the game
+        // return his/her evaluated score
+        if (score == -10)
+            return score;
+
+        // If there are no more moves and
+        // no winner then it is a tie
+        if (isMovesLeft(board) == false)
+            return 0;
+
+        // If this maximizer's move
+        if (isMax)
+        {
+            int best = -1000;
+
+            // Traverse all cells
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    // Check if cell is empty
+                    if (board[i][j]=='\0')
+                    {
+                        // Make the move
+                        board[i][j] = 'X';
+
+                        // Call minimax recursively and choose
+                        // the maximum value
+                        best = Math.max(best, minimax(board,
+                                depth + 1, !isMax));
+
+                        // Undo the move
+                        board[i][j] = '\0';
+                    }
+                }
+            }
+            return best;
+        }
+
+        // If this minimizer's move
+        else
+        {
+            int best = 1000;
+
+            // Traverse all cells
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    // Check if cell is empty
+                    if (board[i][j] == '\0')
+                    {
+                        // Make the move
+                        board[i][j] = 'O';
+
+                        // Call minimax recursively and choose
+                        // the minimum value
+                        best = Math.min(best, minimax(board,
+                                depth + 1, !isMax));
+
+                        // Undo the move
+                        board[i][j] = '\0';
+                    }
+                }
+            }
+            return best;
+        }
+    }
+
+    public int evaluate(char b[][])
+    {
+        // Checking for Rows for X or O victory.
+        for (int row = 0; row < 3; row++)
+        {
+            if (b[row][0] == b[row][1] &&
+                    b[row][1] == b[row][2])
+            {
+                if (b[row][0] == 'X')
+                    return +10;
+                else if (b[row][0] == 'O')
+                    return -10;
+            }
+        }
+
+        // Checking for Columns for X or O victory.
+        for (int col = 0; col < 3; col++)
+        {
+            if (b[0][col] == b[1][col] &&
+                    b[1][col] == b[2][col])
+            {
+                if (b[0][col] == 'X')
+                    return +10;
+
+                else if (b[0][col] == 'O')
+                    return -10;
+            }
+        }
+
+        // Checking for Diagonals for X or O victory.
+        if (b[0][0] == b[1][1] && b[1][1] == b[2][2])
+        {
+            if (b[0][0] == 'X')
+                return +10;
+            else if (b[0][0] == 'O')
+                return -10;
+        }
+
+        if (b[0][2] == b[1][1] && b[1][1] == b[2][0])
+        {
+            if (b[0][2] == 'X')
+                return +10;
+            else if (b[0][2] == 'O')
+                return -10;
+        }
+
+        // Else if none of them have won then return 0
+        return 0;
+    }
+
 
     public int checkWin(int x, int y)   {
 
